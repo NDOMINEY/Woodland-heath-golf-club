c94693e0e7f5a8335cc41452f922a82f61ed25f0
NDOMINEY <>
2023-06-18 20:58:14 +0000

    Add login function to login.html
    - Sucessful login return user to home page
    - Unsucessful login display try again message
    - Tested locally

--- users/views.py
+++ users/views.py
@@ -6,4 +6,22 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'auth/login.html')
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')
+
+        else:
+            messages.success(
+                request, "There was an error logging in, please try again.")
+            return redirect('login')
+
+    else:
+        return render(request, 'auth/login.html')
