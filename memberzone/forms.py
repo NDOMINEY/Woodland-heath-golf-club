82904ff3e5d15d4668ba8f9f52331e0e22c217ae
NDOMINEY <>
2023-06-21 10:00:58 +0000

    Add styling to timeslot options in booking form
    - Add JS to for radio active to change style with class
    - Add button styling to CSS
    - Test form submission works

--- memberzone/forms.py
+++ memberzone/forms.py
@@ -11,6 +11,14 @@ class MakeBooking(ModelForm):
         fields = ('booking_date', 'booking_time',
                   'number_players')
         widgets = {
-            'booking_date': forms.widgets.DateInput(attrs={'type': 'date'}),
-            'booking_time': forms.RadioSelect,
+            'booking_date': forms.widgets.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'booking_time': forms.widgets.RadioSelect(attrs={'class': 'hidden-radio'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(MakeBooking, self).__init__(*args, **kwargs)
+
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field and isinstance(field, forms.TypedChoiceField):
+                field.choices = field.choices[1:]
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -8,3 +8,16 @@ $(document).ready(function () {
         $("#register").addClass("active");
     }
 });
+//
+// $(document).ready(function () {
+//     $('ul.hidden-radio li').addClass("btn btn-primary active");
+//     $('ul.hidden-radio li label').attr('data-toggle', 'dropdown');
+// });
+
+$(document).ready(function () {
+    $('ul.hidden-radio input:radio').change(function () {
+        // Only remove the class in the specific `box` that contains the radio
+        $('li.pressed').removeClass('pressed');
+        $(this).closest('li').addClass('pressed');
+    });
+});
