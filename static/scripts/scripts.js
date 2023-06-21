2b7c1ce9c3a31611116653a32b4f9de433510976
NDOMINEY <>
2023-06-21 11:04:08 +0000

    Add validation in JS to check future booking date
    - Hide rest of form when date select is prior to tomorrow
    - Tested locally

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -8,16 +8,37 @@ $(document).ready(function () {
         $("#register").addClass("active");
     }
 });
-//
-// $(document).ready(function () {
-//     $('ul.hidden-radio li').addClass("btn btn-primary active");
-//     $('ul.hidden-radio li label').attr('data-toggle', 'dropdown');
-// });
 
 $(document).ready(function () {
     $('ul.hidden-radio input:radio').change(function () {
         // Only remove the class in the specific `box` that contains the radio
         $('li.pressed').removeClass('pressed');
         $(this).closest('li').addClass('pressed');
+    });
+});
+
+$(document).ready(function () {
+    let date = $("#id_booking_date").val();
+
+    $('#id_booking_date').change(function () {
+        let newdate = $("#id_booking_date").val();
+
+        if (newdate < date) {
+            $("ul.hidden-radio").addClass("d-none");
+            $("#error_container").removeClass("d-none");
+            $("#date_error").text("Please select date from tomorrow onwards...");
+            $("label[for='id_booking_time_0']").addClass("d-none");
+            $("label[for='id_number_players']").addClass("d-none");
+            $("#id_number_players").addClass("d-none");
+            $("#booking_submit").addClass("d-none");
+        } else {
+            $("ul.hidden-radio").removeClass("d-none");
+            $("label[for='id_number_players']").removeClass("d-none");
+            $("#id_number_players").removeClass("d-none");
+            $("#booking_submit").removeClass("d-none");
+            $("label[for='id_booking_time_0']").removeClass("d-none");
+            $("#error_container").addClass("d-none");
+
+        }
     });
 });
