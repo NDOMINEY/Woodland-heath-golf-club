3df8668417bc5df7a89136b6931124bbaa64efa3
NDOMINEY <>
2023-06-26 18:53:43 +0000

    Add link in base.html to about page

--- general/urls.py
+++ general/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from . views import home_view, contact_us
+from . views import home_view, contact_us, about
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('contact', contact_us, name='contact_us')
+    path('contact', contact_us, name='contact_us'),
+    path('about', about, name='about'),
 
 ]
--- general/views.py
+++ general/views.py
@@ -9,12 +9,16 @@ def home_view(request):
     return render(request, 'general/home.html')
 
 
+def about(request):
+    return render(request, 'general/about.html')
+
+
 def contact_us(request):
     user = request.user
 
     if request.method == "POST":
 
-        messages.success(request, ('Thank you for your online enquiry, we will \
+        messages.success(request, ('Thank you for your online enquiry, we will\
             contact you within the next 1-2 working days'))
 
         return render(request, 'general/home.html')
